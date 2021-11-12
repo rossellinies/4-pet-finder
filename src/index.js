e03a3c041c6654f5206abd387ef3576477b7c60d
Rossellinie Sanchez <rosse.nie57@gmail.com>
2021-11-11 21:40:55 -0500

    added validation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import Modal from 'react-modal';
+
+import { listPets, createPet } from "./api";
+
+import Pet from './Pet';
+import NewPetModal from './NewPetModal';
+
 import "./index.css";
 
 const App = () => {
+const [pets, setPets] = useState([]);
+const [isLoading, setLoading] = useState([]);
+const [isNewPetOpen, setNewPetOpen] = useState(false);
+
+useEffect(() => {
+  setLoading(true);
+  listPets()
+    .then((pets) => setPets(pets))
+    .finally(() => setLoading(false));
+}, []);
+
+const addPet = async (pet) => {
+  return createPet(pet).then((newPet) => {
+    setPets([...pets, newPet]);
+    setNewPetOpen(false);
+  });
+};
+
   return (
     <main>
       <h1>Adopt-a-Pet</h1>
-      <ul>
-        <li>pets go here</li>
-      </ul>
-      <button>Add a Pet</button>
+{ isLoading ? (
+  <div className='loading'>Loading...</div>
+) : (
+    <>
+    <ul>
+    {pets.map ( (pet) => (
+      <li key={pet.id}>
+        <Pet pet={pet} />
+      </li>
+    ))}
+  </ul>
+  <button onClick={() => setNewPetOpen(true)}>Add a Pet</button>
+  </>
+) }
+
+  {isNewPetOpen && (
+    <NewPetModal
+      // isOpen={isNewPetOpen}
+      onSave={addPet}
+      onCancel={() => setNewPetOpen(false)}
+    />
+  )}
     </main>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// ReactDOM.render(<App />, document.querySelector("#root"));
+const el = document.querySelector("#root");
+Modal.setAppElement(el);
+ReactDOM.render(<App />, el);
